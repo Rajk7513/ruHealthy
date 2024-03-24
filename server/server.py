08b3431c5ef1f5ba9843e5fe4ca925d0f9f5cbbd
Manish Bairy <bmanish2908@gmail.com>
2024-03-24 02:18:29 -0400

    imported all the servers in the server

--- server/server.py
+++ server/server.py
@@ -1,5 +1,18 @@
 from flask import Flask
 
+from Campus.Busch.dining_hall.dh_breakfast import get_busch_dh_breakfast_items
+from Campus.Busch.dining_hall.dh_lunch import get_busch_dh_lunch_items
+from Campus.Busch.dining_hall.dh_dinner import get_busch_dh_dinner_items
+
+from Campus.College_Ave.Atrium.atrium_breakfast import get_atrium_breakfast_items
+from Campus.College_Ave.Atrium.atrium_lunch import get_atrium_lunch_items
+from Campus.College_Ave.Atrium.atrium_dinner import get_atrium_dinner_items
+
+from Campus.Livingston.dining_hall.dh_breakfast import get_livi_breakfast_items
+from Campus.Livingston.dining_hall.dh_lunch import get_livi_lunch_items
+from Campus.Livingston.dining_hall.dh_dinner import get_livi_dinner_items
+
+
 app = Flask(__name__)
 
 @app.route('/')
